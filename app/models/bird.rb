ea033a0b80d3ad92e886f48022f93bf6aba3e56a
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-29 00:34:16 +0300

    COMPLETED: Controller Validations

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,8 @@
 class Bird < ApplicationRecord
+
+    #adding validation so that we don't end up with bad data 
+    # validates :name, :species,  presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
+
+
 end
